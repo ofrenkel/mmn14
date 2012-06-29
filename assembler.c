2f00be2ae36978510f66401f2e19a1020786ed45
Bubu1 <d.mosquito@gmail.com>
2012-06-29 15:17:59 +0300

    pure joy

---
+++ assembler.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+#include "types.h"
+
+void main(int argc, char **argv) {
+
+}
--- types.h
+++ types.h
@@ -1,6 +1,11 @@
+typedef boolean int;
+#define TRUE 1
+#define FALSE 0
+
 struct {
 	char *name;
 	int addr;
+	boolean is_data; 
 } Label;
 
 typedef word char[2];
